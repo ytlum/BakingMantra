14a97cfddf28edc483087956cb8ed8694009a312
ytlum <lumyitong@gmail.com>
2025-09-20 18:25:59 +0800

    add collectibles for level 1 & 3

--- Scripts/collectible.gd
+++ Scripts/collectible.gd
@@ -1,6 +1,6 @@
 extends Area2D
 
-@export var item_name: String = "ingredient"   # type of item (set per collectible in Inspector)
+@export var item_name: String = "ingredient"  
 
 func _ready():
 	connect("body_entered", Callable(self, "_on_body_entered"))
@@ -9,4 +9,4 @@ func _on_body_entered(body: Node):
 	if body.is_in_group("player"):   
 		if body.has_method("collect_item"):
 			body.collect_item(item_name)
-		queue_free()   # remove collectible after pickup
+		queue_free()   

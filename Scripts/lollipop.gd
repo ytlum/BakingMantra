6c8fc6b41265c4a599acbd88ddd42c67a886c56b
qiaoxin <qiaoxin8813@1utar.my>
2025-09-21 05:24:56 +0800

    update lolipop

--- Scripts/lollipop.gd
+++ Scripts/lollipop.gd
@@ -62,6 +62,6 @@ func _on_body_entered(body):
 		return
 	
 	# Handle player collision
-	if body.is_in_group("player") and body.has_method("stun_player"):
+	if "Player" in body.name and body.has_method("freeze_player"):
 		body.stun_player(stun_duration)
 		print("Player stunned!")
